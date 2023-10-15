9cd61d9c04b1ddd437b6cab2bf7bd7193a90c63b
Rin <yb6790@rit.edu>
2023-10-15 13:39:53 -0400

    change material when a door is open (test version for deliverable1) , add hint for the interaction

--- Assets/Scripts/Interaction/OpenDoorSwitch.cs
+++ Assets/Scripts/Interaction/OpenDoorSwitch.cs
@@ -4,9 +4,16 @@ using CharacterController = Core.Player.CharacterController;
 
 public class OpenDoorSwitch : InteractiveObject
 {
+    //Testing: change color if door is opened
+    public GameObject Door;
+    public Material newMaterial;
+
     public override bool OnInteractionStart(CharacterController controller)
     {
-        Debug.Log($"{controller.name} interacted with Door {name}");
+        Renderer renderer = Door.GetComponent<Renderer>();
+        renderer.material = newMaterial;
+
+        Debug.Log($"{controller.name} opened the door!");
         return true;
     }
 }

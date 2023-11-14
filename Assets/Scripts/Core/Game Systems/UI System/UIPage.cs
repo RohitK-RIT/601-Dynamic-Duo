dadb631c93f8929951157c4c93614df72a123e0f
Rohit Kulkarni <rk3507@g.rit.edu>
2023-11-14 16:13:53 -0500

    UIPage error fixed

--- Assets/Scripts/Core/Game Systems/UI System/UIPage.cs
+++ Assets/Scripts/Core/Game Systems/UI System/UIPage.cs
@@ -7,7 +7,7 @@ namespace Core.Game_Systems.UI_System
         public abstract PageID PageID { get; }
 
         protected UISystem UISystem { get; private set; }
-        public bool Active => gameObject.activeSelf;
+        public bool Active => gameObject && gameObject.activeSelf;
 
         public virtual void Init(UISystem owner)
         {
